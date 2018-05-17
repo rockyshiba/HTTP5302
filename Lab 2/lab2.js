96fa72337d4971cb3f086d25a403322b210e6905
LeeS <leejsitu@gmail.com>
2018-05-17 10:42:06 -0400

    Part of Lab 2

--- Lab 2/lab2.js
+++ Lab 2/lab2.js
@@ -1,18 +1,18 @@
 //enums
 var months;
 (function (months) {
-    months[months["January"] = 0] = "January";
-    months[months["February"] = 1] = "February";
-    months[months["March"] = 2] = "March";
-    months[months["April"] = 3] = "April";
-    months[months["May"] = 4] = "May";
+    months[months["Janvier"] = 0] = "Janvier";
+    months[months["Fevrier"] = 1] = "Fevrier";
+    months[months["Mars"] = 2] = "Mars";
+    months[months["Avril"] = 3] = "Avril";
+    months[months["Mai"] = 4] = "Mai";
     months[months["June"] = 5] = "June";
-    months[months["July"] = 6] = "July";
-    months[months["August"] = 7] = "August";
-    months[months["September"] = 8] = "September";
-    months[months["October"] = 9] = "October";
-    months[months["November"] = 10] = "November";
-    months[months["December"] = 11] = "December";
+    months[months["Julliet"] = 6] = "Julliet";
+    months[months["Aout"] = 7] = "Aout";
+    months[months["Septembre"] = 8] = "Septembre";
+    months[months["Octobre"] = 9] = "Octobre";
+    months[months["Novembre"] = 10] = "Novembre";
+    months[months["Decembre"] = 11] = "Decembre";
 })(months || (months = {}));
 var days;
 (function (days) {
@@ -30,6 +30,9 @@ console.log('January: ' + months.January);
 console.log('Month 1: ' + months[1]);
 //html elements
 var pTodayDate = document.getElementById("p--today-date");
+var buttonBirthday = document.getElementById("button--birthday");
+var inputDatePicker = document.getElementById("input--date-picker");
+var pBirthdayMessage = document.getElementById("p--birthday-message");
 //Today's date
 var today = new Date();
 console.log(today);
@@ -44,4 +47,24 @@ console.log(today.getDate());
 //today's year
 console.log(today.getFullYear());
 //display today's date to page
-pTodayDate.innerHTML = "Today is " + todayDayOfWeek + ", " + todayMonth + " " + today.getDate() + ", " + today.getFullYear() + " ";
+pTodayDate.innerHTML = "Aujour d'hui c'est " + todayDayOfWeek + ", le " + todayMonth + " de " + today.getDate() + ", " + today.getFullYear() + " annee";
+///Part 2
+var userBirthday;
+//button functionality
+buttonBirthday.onclick = function () {
+    //assign userBrithday value to input value
+    userBirthday = inputDatePicker.value;
+    console.log(userBirthday);
+    var userBirthdayDate = new Date(userBirthday + " GMT-0400");
+    console.log(userBirthdayDate);
+    pBirthdayMessage.innerHTML = constructDateString(userBirthdayDate);
+};
+function constructDateString(inputDate) {
+    //return 'Happy Birthday' if the user's birthday is today
+    if ((inputDate.getMonth() === today.getMonth()) && (inputDate.getDate() === today.getDate())) {
+        return "Happy birthday!";
+    }
+    //return the date of the birthday of the current year
+    var currentYearBirthday = new Date(today.getFullYear + "-" + inputDate.getMonth() + "-" + inputDate.getDate() + " GMT-0400");
+    return "Your birthday is on " + days[currentYearBirthday.getDay()] + " " + months[currentYearBirthday.getMonth()] + " " + currentYearBirthday.getDate() + ", " + today.getFullYear();
+}
--- Lab 2/lab2.ts
+++ Lab 2/lab2.ts
@@ -1,17 +1,17 @@
 //enums
 enum months{
-    January,
-    February,
-    March,
-    April,
-    May,
+    Janvier,
+    Fevrier,
+    Mars,
+    Avril,
+    Mai,
     June,
-    July,
-    August,
-    September,
-    October,
-    November,
-    December
+    Julliet,
+    Aout,
+    Septembre,
+    Octobre,
+    Novembre,
+    Decembre
 }
 
 enum days{
@@ -31,6 +31,9 @@ console.log('Month 1: ' + months[1]);
 
 //html elements
 let pTodayDate = document.getElementById("p--today-date");
+let buttonBirthday = document.getElementById("button--birthday");
+let inputDatePicker = document.getElementById("input--date-picker");
+let pBirthdayMessage = document.getElementById("p--birthday-message");
 
 //Today's date
 let today : Date = new Date();
@@ -51,4 +54,35 @@ console.log(today.getDate());
 console.log(today.getFullYear());
 
 //display today's date to page
-pTodayDate.innerHTML = `Today is ${ todayDayOfWeek }, ${ todayMonth } ${ today.getDate() }, ${ today.getFullYear() } `;
+pTodayDate.innerHTML = `Aujour d'hui c'est ${ todayDayOfWeek }, le ${ todayMonth } de ${ today.getDate() }, ${ today.getFullYear() } annee`;
+
+
+///Part 2
+let userBirthday : string;
+
+//button functionality
+buttonBirthday.onclick = function(){
+
+    //assign userBrithday value to input value
+    userBirthday = inputDatePicker.value;
+
+    console.log(userBirthday);
+
+    let userBirthdayDate : Date = new Date(userBirthday + " GMT-0400");
+    console.log(userBirthdayDate);
+
+    pBirthdayMessage.innerHTML = constructDateString(userBirthdayDate);
+}
+
+
+function constructDateString(inputDate : Date) : string{
+    //return 'Happy Birthday' if the user's birthday is today
+    if( (inputDate.getMonth() === today.getMonth()) && (inputDate.getDate() === today.getDate()) ){
+        return `Happy birthday!`;
+    }
+
+    //return the date of the birthday of the current year
+    let currentYearBirthday : Date = new Date(`${today.getFullYear}-${inputDate.getMonth()}-${inputDate.getDate()} GMT-0400`);
+
+    return `Your birthday is on ${days[currentYearBirthday.getDay()]} ${months[currentYearBirthday.getMonth()]} ${currentYearBirthday.getDate()}, ${today.getFullYear()}`;
+}
